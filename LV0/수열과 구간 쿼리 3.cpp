b9f56d3af2181a3358b095a76b3abdd53fd12f99
June <limyoonjune03@gmail.com>
2025-03-22 17:15:07 +0900

    20250322

--- LV0/수열과 구간 쿼리 3.cpp
+++ LV0/수열과 구간 쿼리 3.cpp
@@ -5,8 +5,13 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
-    vector<int> answer;
-    return answer;
+    for(vector<int> query:queries){
+        int temp = arr[query[0]];
+        arr[query[0]] = arr[query[1]];
+        arr[query[1]] = temp;
+    }
+
+    return arr;
 }
 
 // test function
@@ -15,6 +20,7 @@ int main(void){
     vector<int> arr = {0, 1, 2, 3, 4};
     vector<vector<int>> queire = {{0, 3},{1, 2},{1, 4}};
 
+    arr = solution(arr, queire);
     for(int i:arr) {cout<<i<<" ";}
     cout<<endl;
     return 0;
